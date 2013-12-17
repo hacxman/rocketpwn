1ab49135995079280f3c972b2b963f4c5c66d54c
Maros Zatko <hacxman@gmail.com>
2013-12-17 18:57:22 +0100

    core.utils: add get_tile_pair_pos

--- core/utils.py
+++ core/utils.py
@@ -1,5 +1,16 @@
 import math
+import random
 
+def get_tile_pair_pos(tile, lvl, tiles):
+    pairs = []
+    for y in range(len(lvl)):
+        for x in range(len(lvl[y])):
+            if lvl[x][y] == tile.lower():
+                pairs.append((x, y))
+    p = pairs[random.randrange(0, len(pairs))]
+    for t in tiles:
+        if t.coords == p:
+            return t.rect
 
 def rot_point(point, pivot, angle):
     rads = math.radians(angle)
